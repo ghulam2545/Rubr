3020b8ce50e2484d5984baa2957ea8ba123b44ec
ghulam2545 <ghulam2545@gmail.com>
2022-12-11 17:27:51 +0530

    some styling thing

--- public/app.js
+++ public/app.js
@@ -1,11 +1,47 @@
+/**
+ * to test in mobile browser
+ * http://<Local IP Address>:<port number>
+ */
+
 // console.log("Hello World");
 
-const Alert = (msg) => {
-    alert(`Hey ${msg} dude, I'm not working right now. lol`);
-};
+// const Alert = (msg) => {
+//     alert(`Hey ${msg} dude, I'm not working right now. lol`);
+// };
 
-const lang = document.querySelector(".all-lang");
+// const lang = document.querySelector(".all-lang");
 
-lang.addEventListener("change", () => {
-    Alert(lang.value);
-});
+// lang.addEventListener("change", () => {
+//     Alert(lang.value);
+// });
+
+// textarea = document.querySelector("textarea");
+// textarea.addEventListener("input", autoResize, false);
+
+// function autoResize() {
+//     console.log(typeof this.style.height);
+//     this.style.maxHeight = "400px";
+//     this.style.maxHeight = this.scrollHeight + "px";
+//     // if (this.style.height >= "400") return;
+// }
+
+// const textarea = document.querySelector('textarea');
+// textarea.style.placeholder = "some placeholder";
+
+// window.onload(() => {
+//     const textarea = document.querySelector("textarea");
+//     console.log(textarea);
+//     textarea.style.placeholder = "some placeholder";
+// });
+
+// const data = `int main() {
+//     cout << "name";
+//     return 0;
+// }`;
+// const func = () => {
+//     const res = document.querySelector("textarea");
+//     console.log(res);
+//     res.innerHTML = data;
+//     console.log("done");
+// };
+// func();
